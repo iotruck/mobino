25e96b493ac0e42ee0ae81c9b2d0024ea061aa98
igor-ferreira-bezerra <igor.bezerra@bandtec.com.br>
2021-10-14 19:27:10 -0300

    Splash Screen

--- app/src/main/java/br/com/iotruck/mobino/MainActivity.kt
+++ app/src/main/java/br/com/iotruck/mobino/MainActivity.kt
@@ -1,11 +1,28 @@
 package br.com.iotruck.mobino
 
+import android.app.Activity
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val handle = Handler()
+        handle.postDelayed(Runnable() {
+            run() {
+                startLogin()
+            }
+        },2000)
+    }
+
+    fun startLogin() {
+        startActivity(
+            Intent(this, LoginActivity::class.java),
+            ActivityOptions.makeSceneTransitionAnimation(this).toBundle()
+        )
     }
 }
