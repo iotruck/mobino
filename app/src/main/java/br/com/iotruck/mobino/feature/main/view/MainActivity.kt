59c2f32cef2334ad4433e7be1d3744a1acb9c354
jorge-lsb <jorge.barbosa@bandtec.com.br>
2021-10-14 19:25:08 -0300

    Ajustando estrutura de projeto em padrao mvvm

--- app/src/main/java/br/com/iotruck/mobino/feature/main/view/MainActivity.kt
+++ app/src/main/java/br/com/iotruck/mobino/feature/main/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.iotruck.mobino
+package br.com.iotruck.mobino.feature.main.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.iotruck.mobino.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
