b4404e5ee2319bfc11c630f7b089983eff7ec063
gui-brandao-silva <GUILHERME.SSILVA@bandtec.com.br>
2021-10-19 19:46:45 -0300

    ajustando arquivos

--- app/src/main/java/br/com/iotruck/mobino/feature/schedule/view/ScheduleActivity.kt
+++ app/src/main/java/br/com/iotruck/mobino/feature/schedule/view/ScheduleActivity.kt
@@ -1,4 +1,19 @@
-package br.com.iotruck.mobino.feature.schedule
+package br.com.iotruck.mobino.feature.schedule.view
 
-class ScheduleActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import br.com.iotruck.mobino.R
+import br.com.iotruck.mobino.feature.login.model.TruckerLogin
+import br.com.iotruck.mobino.feature.login.services.TruckerService
+import br.com.iotruck.mobino.feature.main.view.MainActivity
+
+class ScheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acitivity_schedule)
+    }
 }
