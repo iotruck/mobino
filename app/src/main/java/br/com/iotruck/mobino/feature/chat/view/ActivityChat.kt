999e78a4ee432845e2ff81320d3593b2a2b48fec
GabrielCarrera <gabriel.carrera@bandtec.com.br>
2021-11-17 21:33:10 -0300

    Ajustando tela de chat

--- app/src/main/java/br/com/iotruck/mobino/feature/chat/view/ActivityChat.kt
+++ app/src/main/java/br/com/iotruck/mobino/feature/chat/view/ActivityChat.kt
@@ -1,8 +1,11 @@
 package br.com.iotruck.mobino.feature.chat.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import br.com.iotruck.mobino.R
+import br.com.iotruck.mobino.feature.schedule.view.ScheduleActivity
 
 class ActivityChat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,8 @@ class ActivityChat : AppCompatActivity() {
         setContentView(R.layout.activity_chat)
     }
 
+    fun goToTravels(v: View) {
+        startActivity(Intent(this, ScheduleActivity::class.java))
+    }
 
 }
