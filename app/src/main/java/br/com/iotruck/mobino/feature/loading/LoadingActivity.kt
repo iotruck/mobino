03ef93dc6b4f16f0726afff420869890921c8300
LucasFelpx <lucas.asantos@bandtec.com.br>
2021-11-29 22:15:43 -0300

    Tela de splash pós login para recuperar dados

---
+++ app/src/main/java/br/com/iotruck/mobino/feature/loading/LoadingActivity.kt
@@ -0,0 +1,12 @@
+package br.com.iotruck.mobino.feature.loading
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.iotruck.mobino.R
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}
